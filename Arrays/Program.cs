a1b5dfd437c9d73f57117d4469a573f3908b2774
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-03 20:34:23 +0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+
+            //int[] num = new int[] { 6, 5, 3, 9 };
+            //int rem = 0;
+            //for (int i = 0; i <num.Length ; i++)
+            //{
+            //    for (int a = i+1; a < num.Length; a++)
+            //    {
+            //        if (num[i]>num[a])
+            //        {
+            //            rem = num[a];
+            //            num[a] = num[i];
+            //            num[i] = rem;
+
+            //        }
+
+            //    }
+            //    Console.WriteLine(num[i]);
+            //}
+
+
+            //Q2
+
+            //int sum = 0;
+            //int[] num = { 3, 5, 7 };
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    sum += num[i];
+            //    sum = sum / num.Length + 1;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+            //Q3
+
+            //int[] num = new int[] { 10, 20, 30, 40, 50 };
+            //for (int i = num.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+        }
+    }
+}
